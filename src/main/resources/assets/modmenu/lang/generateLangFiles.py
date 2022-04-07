0c5f78cb9fb1cc1e3445e615db6447fbff236aac
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-04-07 12:26:49 +0200

    removed one line

--- src/main/resources/assets/modmenu/lang/generateLangFiles.py
+++ src/main/resources/assets/modmenu/lang/generateLangFiles.py
@@ -3,11 +3,10 @@ from pathlib import Path
 
 
 for file in Path( '.' ).glob('*.json'):
-	lang: dict[str, str] = json.loads( file.read_text(encoding='utf-8') )
-	langFile = ''
+	lang = ''
 	
-	for key, value in lang.items():
+	for key, value in json.loads( file.read_text( encoding='utf-8' ) ).items():
 		if 'drop' not in key:
-			langFile += f'{key}={value}\n'
+			lang += f'{key}={value}\n'
 	
-	( file.parent / ( file.name.removesuffix('json') + 'lang' ) ).write_text( langFile, encoding='utf-8' )
+	( file.parent / ( file.name.removesuffix('json') + 'lang' ) ).write_text( lang, encoding='utf-8' )
