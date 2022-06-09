d7f45e5b398027de26d91617716d13b8fb3220bd
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-06-09 17:08:09 +0200

    add forgotter hover behavior

--- src/main/java/io/github/prospector/modmenu/gui/widget/BetterEntryListWidget.java
+++ src/main/java/io/github/prospector/modmenu/gui/widget/BetterEntryListWidget.java
@@ -10,7 +10,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
-public abstract class BetterEntryListWidget< E extends BetterEntryListWidget.Entry<E> > extends EntryListWidget implements Renderable {
+public abstract class BetterEntryListWidget<E extends BetterEntryListWidget.Entry<E>> extends EntryListWidget implements Renderable {
 	private final List<E> children = new Entries();
 	@Nullable
 	private E selected;
@@ -20,16 +20,27 @@ public abstract class BetterEntryListWidget< E extends BetterEntryListWidget.Ent
 	private E focused;
 
 	public BetterEntryListWidget( MinecraftClient minecraftClient, int width, int height, int top, int bottom, int itemHeight ) {
-		super(minecraftClient, width, height, top, bottom, itemHeight);
+		super( minecraftClient, width, height, top, bottom, itemHeight );
 	}
 
-	public void setSelected( @Nullable E entry) {
+	@Override
+	public void render( int mouseX, int mouseY, float delta ) {
+		super.render( mouseX, mouseY, delta );
+		this.hoveredEntry = findEntryAt( mouseX, mouseY );
+	}
+
+	public void setSelected( @Nullable E entry ) {
 		this.selected = entry;
 	}
 
 	@Nullable
 	public E getSelectedOrNull() {
 		return this.selected;
+	}
+
+	@Nullable
+	public E getHoveredEntryOrNull() {
+		return this.hoveredEntry;
 	}
 
 	@Nullable
@@ -49,17 +60,17 @@ public abstract class BetterEntryListWidget< E extends BetterEntryListWidget.Ent
 		this.children.clear();
 	}
 
-	protected void replaceEntries(Collection<E> newEntries) {
+	protected void replaceEntries( Collection<E> newEntries ) {
 		this.children.clear();
-		this.children.addAll(newEntries);
+		this.children.addAll( newEntries );
 	}
 
-	public E getEntry(int index) {
-		return this.children().get(index);
+	public E getEntry( int index ) {
+		return this.children().get( index );
 	}
 
-	protected int addEntry(E entry) {
-		this.children.add(entry);
+	protected int addEntry( E entry ) {
+		this.children.add( entry );
 		return this.children.size() - 1;
 	}
 
@@ -67,32 +78,38 @@ public abstract class BetterEntryListWidget< E extends BetterEntryListWidget.Ent
 		return this.children().size();
 	}
 
-	protected boolean isSelectedEntry(int index) {
-		return Objects.equals(this.getSelectedOrNull(), this.children().get(index));
+	protected boolean isSelectedEntry( int index ) {
+		return Objects.equals( this.getSelectedOrNull(), this.children().get( index ) );
+	}
+
+	public @Nullable E findEntryAt( int x, int y ) {
+		int index = getEntryAt( x, y );
+		return index != -1 ? this.getEntry( index ) : null;
 	}
 
 	@Nullable
-	public E remove(int index) {
-		E entry = this.children.get(index);
-		return this.removeEntry( this.children.get(index) ) ? entry : null;
+	public E remove( int index ) {
+		return this.removeEntry( this.children.get( index ) ) ?
+			this.children.get( index ) :
+			null;
 	}
 
-	protected boolean removeEntry(E entry) {
-		boolean bl = this.children.remove(entry);
-		if ( bl && entry == this.getSelectedOrNull() ) {
+	protected boolean removeEntry( E entry ) {
+		boolean bl = this.children.remove( entry );
+		if ( bl && entry == this.getSelectedOrNull() )
 			this.setSelected( null );
-		}
+
 		return bl;
 	}
 
-	public boolean isMouseOver(double mouseX, double mouseY) {
+	public boolean isMouseOver( double mouseX, double mouseY ) {
 		return mouseY >= (double) this.yStart &&
-				mouseY <= (double) this.yEnd &&
-				mouseX >= (double) this.xStart &&
-				mouseX <= (double) this.xStart;
+			mouseY <= (double) this.yEnd &&
+			mouseX >= (double) this.xStart &&
+			mouseX <= (double) this.xStart;
 	}
 
-	void setEntryParentList(Entry<E> entry ) {
+	void setEntryParentList( Entry<E> entry ) {
 		entry.parentList = this;
 	}
 
@@ -101,31 +118,31 @@ public abstract class BetterEntryListWidget< E extends BetterEntryListWidget.Ent
 
 		Entries() { }
 
-		public E get(int i) {
-			return (E)this.entries.get(i);
+		public E get( int i ) {
+			return this.entries.get( i );
 		}
 
 		public int size() {
 			return this.entries.size();
 		}
 
-		public E set(int i, E entry) {
-			E entry2 = (E)this.entries.set(i, entry);
-			BetterEntryListWidget.this.setEntryParentList(entry);
+		public E set( int i, E entry ) {
+			E entry2 = this.entries.set( i, entry );
+			BetterEntryListWidget.this.setEntryParentList( entry );
 			return entry2;
 		}
 
-		public void add(int i, E entry) {
-			this.entries.add(i, entry);
-			BetterEntryListWidget.this.setEntryParentList(entry);
+		public void add( int i, E entry ) {
+			this.entries.add( i, entry );
+			BetterEntryListWidget.this.setEntryParentList( entry );
 		}
 
-		public E remove(int i) {
-			return (E)this.entries.remove(i);
+		public E remove( int i ) {
+			return this.entries.remove( i );
 		}
 	}
 
-	public static abstract class Entry< E extends Entry<E> > implements EntryListWidget.Entry {
+	public static abstract class Entry<E extends Entry<E>> implements EntryListWidget.Entry {
 		BetterEntryListWidget<E> parentList;
 
 		public Entry() { }
@@ -133,8 +150,7 @@ public abstract class BetterEntryListWidget< E extends BetterEntryListWidget.Ent
 		public abstract void render( int index, int y, int x, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean hovered, float tickDelta );
 
 		public boolean isMouseOver( int mouseX, int mouseY ) {
-			//noinspection EqualsBetweenInconvertibleTypes
-			return Objects.equals( this.parentList.getEntryAt( mouseX, mouseY ), this );
+			return Objects.equals( this.parentList.findEntryAt( mouseX, mouseY ), this );
 		}
 	}
 
